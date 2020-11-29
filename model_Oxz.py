55c960a6dacd5ce84b04aca139c037ed2c739787
Mashin Nikita <mashin.na@phystech.edu>
2020-11-29 19:47:33 +0300

    Пофиксил модуль Oxz

--- model_Oxz.py
+++ model_Oxz.py
@@ -15,11 +15,15 @@ gravitational_constant = 1
 def body_move(body, dt):
     ax = body.Fx / body.m
     body.Vx += ax*dt
-    body.x += body.Vx * dt
+    body.x += body.Vx * dt + ax*dt**2/2
 
     ay = body.Fy / body.m
     body.Vy += ay*dt
-    body.y += body.Vy * dt
+    body.y += body.Vy * dt + ay*dt**2/2
+
+    az = body.Fz / body.m
+    body.Vz += az*dt
+    body.z += body.Vy * dt + az*dt**2/2
 
 
 def sum_of_squares(v):
@@ -35,23 +39,28 @@ def body_force(body, objects):
             continue
         if obj.type == "smallbody":
             continue
-        vec = np.array([obj.x - body.x, obj.y - body.y])
+        vec = np.array([obj.x - body.x, obj.y - body.y, obj.z - body.z])
         r = np.sqrt(sum_of_squares(vec))
         unit_vec = vec / r
 
         df = gravitational_constant * body.m * obj.m / r ** 2
         body.Fx += df * unit_vec[0]
         body.Fy += df * unit_vec[1]
-
+        body.Fz += df * unit_vec[2]
 
 def recalculate_objects_positions(objects, dt):
     for body in objects:
+        if body.type == "bigbody":
+            body.x = body.x
+            body.y = 0
+            body.z = 0
+        if body.type == "smallbody":
+            body.x = 0
+            body.y = body.z 
+    for body in objects:
         body_force(body, objects)
     for body in objects:
-        if body.type == "bigbody":
-            body_move(body, dt)
-        else:
-            body_move(body, dt)
+        body_move(body, dt)
 
 
 if __name__ == "__main__":
