28576ce55a17ff84fb4b31e003af60f58a6029fe
KleimenovM <kleimenov.max@phystech.edu>
2020-12-15 12:43:34 +0300

    bibbody comments

--- objects.py
+++ objects.py
@@ -12,20 +12,21 @@ class BigBody:
     """Class that describes body with big mass"""
     def __init__(self):
         self.type = "big body"
-        self.m = 1000
-        self.R = 10
+        self.m = 1000  # mass proportional to unit mass (1) of small body
+        self.R = 10  # radius in px
         self.color = "red"
+        # body image is a circle of self.R and self.color
+        # It generates in window module
         self.image = None
-        self.path = []
 
-        self.x, self.y = 0, 0
+        self.x, self.y = 0, 0  # visual coordinates of the body
 
-        self.r = np.array([0, 0, 0])
-        self.V = np.array([0, 0, 0])
-        self.F = np.array([0, 0, 0])
+        self.r = np.array([0, 0, 0])  # (x, y, z)
+        self.V = np.array([0, 0, 0])  # (Vx, Vy, Vz)
+        self.F = np.array([0, 0, 0])  # (Fx, Fy, Fz)
 
-        self.vel_0 = (0, 0, 0)
-        self.vec_0 = (0, 0, 0)
+        self.vel_0 = (0, 0, 0)  # initial velocity vector
+        self.vec_0 = (0, 0, 0)  # initial radius vector
 
     def push(self, event):
         """
@@ -45,7 +46,7 @@ class SmallBody:
     """Class that describes body with small mass"""
     def __init__(self):
         self.type = "small body"
-        self.m = 1
+        self.m = 0.01
         self.R = 5
         self.color = "#c00"
         self.image = None
